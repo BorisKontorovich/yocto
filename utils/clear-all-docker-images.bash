053cbac830f71936ec40d28f0c52a99b52140f3f
BorisKontorovich <boris@kontorovich.com>
2024-08-21 18:07:10 -0400

    Renamed.

--- utils/clear-all-docker-images.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# sudo docker stop $(sudo docker ps -a -q) && \
-sudo docker rm $(sudo docker ps -aq) && \
-sudo docker rmi -f $(sudo docker images -a) && \
-sudo docker builder prune && \
-sudo docker system prune --volumes
---
+++ utils/docker-clear.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# docker stop $(sudo docker ps -a -q) && \
+# docker rm $(sudo docker ps -aq) && \
+docker rmi -f $(sudo docker images -a) && \
+docker builder prune && \
+docker system prune --volumes
