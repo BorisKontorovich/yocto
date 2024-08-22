45afb8ba43967b50923b69736e97afa251bcb74c
BorisKontorovich <boris@kontorovich.com>
2024-08-22 10:58:28 -0400

    Fixing docker commands.

--- utils/docker-clear.bash
+++ utils/docker-clear.bash
@@ -2,6 +2,10 @@
 
 # docker stop $(sudo docker ps -a -q) && \
 # docker rm $(sudo docker ps -aq) && \
-docker rmi -f $(sudo docker images -a) && \
-docker builder prune && \
-docker system prune --volumes
+# docker rmi -f $(sudo docker images -a) && \
+# docker builder prune && \
+# docker system prune && \
+# docker system prune --volumes
+
+# Remove all images
+docker rmi $(docker images -q)
